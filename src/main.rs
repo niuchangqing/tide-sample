029f31fd38e971e2b41d2d25014c6b4d4a6f6d69
niuchangqing <ncq1993@188.com>
2021-03-18 21:08:52 +0800

    添加rbatis

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+
 mod user;
 mod result_json;
 use result_json::ResultJson;
 mod user_controller;
+use lazy_static::lazy_static;
+use rbatis::rbatis::Rbatis;
+lazy_static! {
+    static ref RB:Rbatis = Rbatis::new();
+}
+
 #[async_std::main]
 async fn main() ->tide::Result<()> {
+    async_std::task::block_on(async { RB.link("mysql://root:123456@localhost:3306/test").await.unwrap() });
     let mut app = tide::new();
     app.at("/user/:id").get(user_controller::find_user_by_id);
     app.listen("127.0.0.1:8080").await?;
